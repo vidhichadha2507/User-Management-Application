ec92afc965398e78659923b94d9f82685538d6e0
vidhichadha2507 <vidhichadha2507@gmail.comgit config --global user.email vidhichadha2507@gmail.comgit config --global user.email vidhichadha2507@gmail.com>
2024-04-19 16:02:28 +0530

    Added login form

---
+++ components/auth/login-form.tsx
@@ -0,0 +1,79 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { CardWrapper } from "./card-wrapper";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+import { LoginSchema } from "@/schemas/index";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
+  FormLabel,
+} from "../ui/form";
+import { Input } from "../ui/input";
+import { Button } from "../ui/button";
+
+export const LoginForm = () => {
+  const form = useForm<z.infer<typeof LoginSchema>>({
+    resolver: zodResolver(LoginSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  return (
+    <CardWrapper
+      headerLabel="Welcome Back!"
+      backButtonLabel="Not yet have an account?"
+      backButtonHref="/auth/register"
+      showSocial
+    >
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(() => {})} className="space-y-6">
+          <div className="space-y-4">
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input
+                      {...field}
+                      placeholder="vidhichadha@gmail.com"
+                      type="email"
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <Input {...field} placeholder="******" type="password" />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+          <Button
+            type="submit"
+            className="w-full bg-blue-400 hover:bg-blue-500 transition-colors duration-200"
+          >
+            Login
+          </Button>
+        </form>
+      </Form>
+    </CardWrapper>
+  );
+};
